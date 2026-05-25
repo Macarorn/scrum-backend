b5c9805f906cad918242ac3ab26e8c4e17689b04
jferrojas99@gmail.com <juan>
2026-05-25 16:02:06 -0500

    Fix calendario and merge conflicts

--- src/routes/criterios.routes.js
+++ src/routes/criterios.routes.js
@@ -1,11 +1,11 @@
 import express from "express";
 import * as criteriosController from "../controllers/criterios.controller.js";
 import { authMiddleware } from "../middleware/auth.middleware.js";
-import { authorizationMiddleware } from "../middleware/authorization.middleware.js";
+import { checkPermission } from "../middleware/authorization.middleware.js";
 
 const router = express.Router();
 
-router.put("/:id", authMiddleware, authorizationMiddleware(["Product Owner", "Scrum Master"]), criteriosController.actualizarCriterio);
-router.delete("/:id", authMiddleware, authorizationMiddleware(["Product Owner", "Scrum Master"]), criteriosController.eliminarCriterio);
+router.put("/:id", authMiddleware, checkPermission("editar_backlog"), criteriosController.actualizarCriterio);
+router.delete("/:id", authMiddleware, checkPermission("editar_backlog"), criteriosController.eliminarCriterio);
 
 export default router;
