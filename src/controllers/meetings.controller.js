e42bbfacdf895adba107f0fa3d628d3df1513e6b
Macarron <marianacifuenteszu@gmail.com>
2026-06-03 20:26:01 -0500

    fix small details

--- src/controllers/meetings.controller.js
+++ src/controllers/meetings.controller.js
@@ -1,7 +1,7 @@
 import pool from "../utils/database.js";
 
 const DAY_IN_MS = 24 * 60 * 60 * 1000;
-const VALID_PRIORITIES = new Set(["alta", "media", "baja"]);
+const VALID_PRIORITIES = new Set(["alta", "media", "baja", "estandar"]);
 
 const startOfDay = (value = new Date()) => {
   const date = value instanceof Date ? new Date(value) : new Date(value);
